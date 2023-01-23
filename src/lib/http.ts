100aeb7f39947ebf9501eb622cfbce0163c309c6
Jaeiya <exanix@gmail.com>
2023-01-22 19:35:17 -0800

    feat(http): handle errors directly inside methods

--- src/lib/http.ts
+++ src/lib/http.ts
@@ -1,8 +1,11 @@
+import { Logger } from './logger.js';
 import { tryCatchAsync } from './utils.js';
 
+const _cc = Logger.consoleColors;
+
 export class HTTP {
-  static post(url: string, body: string) {
-    return tryCatchAsync(
+  static async post(url: string, body: string) {
+    const resp = await tryCatchAsync(
       fetch(url, {
         method: 'POST',
         body,
@@ -11,17 +14,27 @@ export class HTTP {
         },
       })
     );
+    if (resp instanceof Error) {
+      execError(resp, 'POST', url.toString());
+      process.exit(1);
+    }
+    return resp;
   }
 
-  static get(url: URL, token?: string) {
+  static async get(url: URL, token?: string) {
     const headers = token ? { Authorization: `Bearer ${token}` } : null;
-    return tryCatchAsync(
+    const resp = await tryCatchAsync(
       fetch(url, headers ? { method: 'GET', headers } : { method: 'GET' })
     );
+    if (resp instanceof Error) {
+      execError(resp, 'GET', url.toString());
+      process.exit(1);
+    }
+    return resp;
   }
 
-  static patch(url: URL, body: string, token: string) {
-    return tryCatchAsync(
+  static async patch(url: URL, body: string, token: string) {
+    const resp = await tryCatchAsync(
       fetch(url, {
         method: 'PATCH',
         body,
@@ -31,5 +44,16 @@ export class HTTP {
         },
       })
     );
+    if (resp instanceof Error) {
+      execError(resp, 'PATCH', url.toString());
+      process.exit(1);
+    }
+    return resp;
+  }
+}
+
+function execError(resp: Error, method: string, url: string) {
+  if (resp instanceof Error) {
+    Logger.chainError(['', `${_cc.rd}Failed ${method}ing ${url}`, resp.message]);
   }
 }

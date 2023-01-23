bd3e9a893e23bc74534341fb54b174e51e506837
Jaeiya <exanix@gmail.com>
2023-01-22 16:21:44 -0800

    feat(watch): update Kitsu on watch

--- src/lib/watch.ts
+++ src/lib/watch.ts
@@ -1,30 +1,81 @@
-import { existsSync, mkdirSync, readdirSync, renameSync } from 'node:fs';
+import {
+  existsSync,
+  futimes,
+  mkdirSync,
+  readdirSync,
+  renameSync,
+  truncate,
+} from 'node:fs';
+import { CLI } from './cli.js';
+import { KitsuAPI } from './kitsu.js';
 import { Logger } from './logger.js';
-import { fitString, pathJoin, toEpisodeNum } from './utils.js';
+import {
+  fitString,
+  pathJoin,
+  titleFromAnimeFileName,
+  toEpisodeNum,
+  truncateStr,
+} from './utils.js';
 
 const _cc = Logger.consoleColors;
 
-export function watchAnime(epName: string, epNum: string, workingDir: string) {
-  validateParams([epName, epNum, workingDir]);
+export async function watchAnime(
+  epName: string,
+  epNums: [string, string],
+  workingDir: string,
+  kitsu: KitsuAPI
+) {
+  validateParams([epName, epNums, workingDir]);
   Logger.info(`Working directory: ${_cc.bgn}${workingDir}`);
+  const [lookupNum, progressNum] = epNums;
 
   tryCreateWatchedDir(workingDir);
-  const saneEpNum = toEpisodeNum(Number(epNum));
+  const saneEpNum = toEpisodeNum(Number(lookupNum));
   const fansubFileNames = filterFansubs(workingDir, epName, `- ${saneEpNum}`);
 
   validateFileNames(fansubFileNames, epName, saneEpNum);
-  moveFileToWatchedDir(fansubFileNames[0], workingDir);
-
-  Logger.info(
-    `Moved ${_cc.yw}${fitString(
-      fansubFileNames[0].split(`- ${saneEpNum}`)[0].trimEnd(),
-      50
-    )} - ${saneEpNum}`
+  const cachedAnime = kitsu.animeCache.filter((anime) =>
+    anime[1].toLowerCase().includes(epName)
   );
+  if (!cachedAnime.length) {
+    console.log('');
+    Logger.error(`${_cc.rd}Watch List Incomplete`);
+    Logger.error(
+      `${_cc.bcn}Missing:${_cc.x} ${_cc.gn}${titleFromAnimeFileName(
+        fansubFileNames[0],
+        saneEpNum
+      )}`
+    );
+    process.exit(1);
+  }
+  if (cachedAnime.length > 1) {
+    console.log('');
+    Logger.error(`${_cc.rd}Multiple Cached Titles Found`);
+    cachedAnime.forEach((anime) => Logger.error(`${_cc.bcn}Title:${_cc.x} ${anime[1]}`));
+    Logger.error(`${_cc.byw}Use a more unique name to reference the episode`);
+    process.exit(1);
+  }
+  const cachedID = cachedAnime[0][0];
+  Logger.info(`${_cc.bcn}Jap Title:${_cc.x} ${_cc.gn}${cachedAnime[0][1]}`);
+  Logger.info(`${_cc.bcn}Eng Title:${_cc.x} ${_cc.gn}${cachedAnime[0][2]}`);
+  await kitsu.updateAnime(
+    `https://kitsu.io/api/edge/library-entries/${cachedAnime[0][0]}`,
+    {
+      data: {
+        id: cachedID,
+        type: 'library-entries',
+        attributes: {
+          progress: Number(progressNum || lookupNum),
+        },
+      },
+    }
+  );
+  moveFileToWatchedDir(fansubFileNames[0], workingDir);
+  Logger.info(`${_cc.bcn}Moved To:${_cc.x} ${_cc.byw}${pathJoin(workingDir, 'watched')}`);
 }
 
-function validateParams(params: string[]) {
-  const [epName, epNum, workingDir] = params;
+function validateParams(params: [string, string[], string]) {
+  const [epName, epNumbers, workingDir] = params;
 
   if (!existsSync(workingDir)) {
     Logger.error(`Working directory invalid: ${_cc.yw}${workingDir}`);
@@ -36,13 +87,15 @@ function validateParams(params: string[]) {
     process.exit(1);
   }
 
-  if (!epNum) {
+  if (!epNumbers.length) {
     Logger.error('Missing episode number argument');
     process.exit(1);
   }
 
-  if (isNaN(Number(epNum))) {
-    Logger.error(`You passed an invalid episode number: ${_cc.byw}${epNum}`);
+  if (isNaN(Number(epNumbers[0])) || isNaN(Number(epNumbers[1]))) {
+    Logger.error(
+      `You passed an invalid episode number: ${_cc.byw}${epNumbers.join(', ')}`
+    );
     process.exit(1);
   }
 }

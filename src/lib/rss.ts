efe89e730d9ae7be8a145d68ce1ba2f7a4f350ee
Jaeiya <exanix@gmail.com>
2023-02-27 15:52:53 -0800

    feat(rss): add print loader

--- src/lib/rss.ts
+++ src/lib/rss.ts
@@ -1,8 +1,10 @@
 import { HTTP } from './http.js';
 
 const _nyaaURLStr = 'https://nyaa.si';
+const _printLoader = _con.getLoadPrinter();
 
 export async function getFansubRSS(animeName: string) {
+    _printLoader.start(';bg;*** ;by;Looking up RSS ;bg;***');
     const url = new URL(_nyaaURLStr);
     url.searchParams.append('f', '2');
     url.searchParams.append('c', '1_2');
@@ -28,6 +30,7 @@ async function getLatestAnimeEntry(html: string) {
     const numOfResults = $('.success').length;
     const latestTitle =
         els.children('a.comments + a').eq(0).text().trim() || els.children('a').eq(0).text();
+    _printLoader.stop();
     if (!els.length) {
         _con.error('Anime Not Found');
         process.exit(1);

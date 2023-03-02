d7804246bf5b5d3fc3eb3f4ec71536b19e25f381
Jaeiya <exanix@gmail.com>
2023-03-02 15:56:45 -0800

    clean(main): move config init and warning removal to global

--- src/globals.ts
+++ src/globals.ts
@@ -1,3 +1,25 @@
+import { Config } from './lib/config.js';
+import { Kitsu } from './lib/kitsu/kitsu.js';
 import { ConsoleLogger } from './lib/logger.js';
 
+// Fetch creates a warning
+process.removeAllListeners('warning');
 global._con = ConsoleLogger;
+
+await Config.init({
+    setupNewConfig: async () => {
+        _con.info(`;bc;Working Directory: ;g;${process.cwd()}`);
+        await Kitsu.init();
+    },
+    setDefaultProps: (config) => {
+        if (config.kitsu) {
+            config.kitsu.cache ??= [];
+        }
+        if (config.kitsu?.stats) {
+            config.kitsu.stats.secondsSpentWatching ??= 0;
+            config.kitsu.stats.completedSeries ??= 0;
+        }
+        return config;
+    },
+});
+
--- src/main.ts
+++ src/main.ts
@@ -2,25 +2,6 @@
 import './globals.js';
 import { CLI } from './lib/cli/cli.js';
 import { Flags } from './lib/cli/flags/flags.js';
-import { Config } from './lib/config.js';
-import { Kitsu } from './lib/kitsu/kitsu.js';
-
-await Config.init({
-    setupNewConfig: async () => {
-        _con.info(`;bc;Working Directory: ;g;${process.cwd()}`);
-        await Kitsu.init();
-    },
-    setDefaultProps: (config) => {
-        if (config.kitsu) {
-            config.kitsu.cache ??= [];
-        }
-        if (config.kitsu?.stats) {
-            config.kitsu.stats.secondsSpentWatching ??= 0;
-            config.kitsu.stats.completedSeries ??= 0;
-        }
-        return config;
-    },
-});
 
 for (const Flag of Flags) {
     CLI.addFlag(new Flag());

4fc5dd2b732baf36f3997da5cbac3d35a165d504
Jaeiya <exanix@gmail.com>
2023-05-20 15:55:57 -0700

    feat: add Kitsu request URLs and URL API
    This allows us track and build Kitsu request URLs a lot more
    intuitively.

---
+++ src/lib/kitsu/kitsu-url-api.ts
@@ -0,0 +1,61 @@
+export class KitsuUrlAPI {
+    #url: URL;
+
+    constructor(url: string) {
+        this.#url = new URL(url);
+    }
+
+    get url() {
+        return this.#url;
+    }
+
+    setQueryText(text: string) {
+        this.#url.searchParams.append('filter[text]', text);
+        return this;
+    }
+
+    setPageLimit(limit: number) {
+        this.#url.searchParams.append('page[limit]', limit.toString());
+        return this;
+    }
+
+    filterAnimeFields(fields: string[]) {
+        this.#url.searchParams.append(`fields[anime]`, fields.join(','));
+        return this;
+    }
+
+    filterUserName(name: string) {
+        this.#url.searchParams.append('filter[name]', name);
+        return this;
+    }
+
+    filterUserID(id: string) {
+        this.#url.searchParams.append('filter[user_id]', id);
+        return this;
+    }
+
+    filterLibraryID(ids: string[]) {
+        this.#url.searchParams.append('filter[id]', ids.join(','));
+        return this;
+    }
+
+    filterAnimeType(type: 'tv' | 'movie' | 'ova') {
+        this.#url.searchParams.append(`filter[subtype[]`, type);
+        return this;
+    }
+
+    filterStatus(status: 'current' | 'dropped' | 'planned' | 'completed') {
+        this.#url.searchParams.append('filter[status]', status);
+        return this;
+    }
+
+    filterMediaType(type: 'manga' | 'anime') {
+        this.#url.searchParams.append('filter[kind]', type);
+        return this;
+    }
+
+    includeCategory(categories: string[]) {
+        this.#url.searchParams.append('include', categories.join(','));
+        return this;
+    }
+}
---
+++ src/lib/kitsu/kitsu-urls.ts
@@ -0,0 +1,35 @@
+import { Config } from '../config.js';
+import { KitsuUrlAPI } from './kitsu-url-api.js';
+
+export class KitsuURLs {
+    static getAnimeInfoURL(queryText: string) {
+        return new KitsuUrlAPI('https://kitsu.io/api/edge/anime')
+            .setQueryText(queryText)
+            .setPageLimit(5)
+            .filterAnimeType('tv')
+            .filterAnimeFields([
+                'titles',
+                'canonicalTitle',
+                'abbreviatedTitles',
+                'averageRating',
+                'episodeCount',
+                'slug',
+                'synopsis',
+            ]).url;
+    }
+
+    static getLibraryAnimeInfoURL(animeLibraryIDs: string[]) {
+        return new KitsuUrlAPI('https://kitsu.io/api/edge/library-entries')
+            .filterLibraryID(animeLibraryIDs)
+            .includeCategory(['anime'])
+            .filterAnimeFields(['episodeCount', 'averageRating', 'synopsis'])
+            .setPageLimit(200).url;
+    }
+
+    static getWatchListURL() {
+        return new KitsuUrlAPI(Config.getKitsuProp('urls').library)
+            .filterStatus('current')
+            .setPageLimit(200)
+            .includeCategory(['anime']).url;
+    }
+}

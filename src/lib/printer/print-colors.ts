8a62b046f197566c37044dcea12d91e0149d420e
Jaeiya <exanix@gmail.com>
2023-06-01 17:23:17 -0700

    feat: rewrite `Logger{}` from scratch as `Printer{}`
    This removes tags and closes #8
    This adds paragraph utility which closes #9

---
+++ src/lib/printer/print-colors.ts
@@ -0,0 +1,110 @@
+type ColorCode = keyof typeof _consoleColors;
+type HexColor = string;
+
+const _consoleColors = {
+    /** Black */
+    k: '\u001B[30m',
+    /** Bright Black */
+    bk: '\u001B[90m',
+    /** Bright White */
+    bw: '\u001B[97m',
+    /** Red */
+    r: '\u001B[31m',
+    /** Bright Red */
+    br: '\u001B[91m',
+    /** Dark Green */
+    g: '\u001B[32m',
+    /** Bright Green */
+    bg: '\u001B[92m',
+    /** Yellow */
+    y: '\u001B[33m',
+    /** Bright Yellow */
+    by: '\u001B[93m',
+    /** Blue */
+    b: '\u001B[34m',
+    /** Bright Blue */
+    bb: '\u001B[94m',
+    /** Magenta */
+    m: '\u001B[35m',
+    /** Bright Magenta */
+    bm: '\u001B[95m',
+    /** Cyan */
+    c: '\u001B[36m',
+    /** Bright Cyan */
+    bc: '\u001B[96m',
+    /** Default Color */
+    x: '\u001B[39m',
+    /** Default Background Color */
+    xbg: '\u001B[49m',
+    /** Reset All */
+    xx: '\u001B[0m',
+};
+
+let _disableColor = false;
+
+const _colorCodeMap = (function () {
+    const map = new Map<string, string>();
+    for (const key in _consoleColors) {
+        const k = key as ColorCode;
+        map.set(k, _consoleColors[k]);
+    }
+    return map;
+})();
+
+export class PrinterColor {
+    static disableColor() {
+        _disableColor = true;
+    }
+
+    static enableColor() {
+        _disableColor = false;
+    }
+
+    static colorText(text: string) {
+        return replaceColorCodes(text, _disableColor ? '' : undefined);
+    }
+
+    static stripColorCodes(text: string) {
+        return replaceColorCodes(text, '');
+    }
+
+    static addCustomColor(code: string, color: HexColor) {
+        const [r, g, b] = toRGBFromHex(color);
+        if (code.length > 3) {
+            throw Error('color code must be between 1 and 3 characters long');
+        }
+        if (_colorCodeMap.has(code)) {
+            throw Error(`color code "${code}" already exists`);
+        }
+        _colorCodeMap.set(code, `\u001B[38;2;${r};${g};${b}m`);
+    }
+}
+
+function replaceColorCodes(text: string, replacement?: string) {
+    const colorCodes = text.match(/;([a-z]){1,3};/g);
+    if (!colorCodes) return text;
+
+    const invalidCodes = colorCodes.filter((c) => !_colorCodeMap.has(c.replaceAll(';', '')));
+    if (invalidCodes.length) {
+        throw Error(`invalid color code(s) "${invalidCodes}"`);
+    }
+
+    for (const [code, color] of _colorCodeMap) {
+        const colorCode = `;${code};`;
+        if (text.includes(colorCode)) {
+            text = text.replaceAll(colorCode, replacement ?? color);
+        }
+    }
+    return text;
+}
+
+function toRGBFromHex(hex: string) {
+    // Match hex codes: FF00FF or FFF case insensitive
+    const hexMatcher = /^[0-9a-f]{6}|[0-9a-f]{3}$/gi;
+    if (!hexMatcher.test(hex)) {
+        throw Error('invalid hex color');
+    }
+    const fullHex = hex.length > 3 ? hex : [...hex].map((c) => c + c).join('');
+    const intFromHex = parseInt(fullHex, 16);
+    return [(intFromHex >> 16) & 0xff, (intFromHex >> 8) & 0xff, intFromHex & 0xff];
+}
---
+++ src/lib/printer/printer.ts
@@ -0,0 +1,217 @@
+import { createSpinner } from '../cli/cli-spinner.js';
+import { pathBasename } from '../utils.js';
+import { PrinterColor } from './print-colors.js';
+import { inspect } from 'util';
+
+export type Log =
+    | LogBasic
+    | LogHeader
+    | LogParagraph
+    | LogCommandDefinition
+    | LogSyntax
+    | LogCommandExample
+    | null;
+
+type LogBasic = [kind: '', message: string];
+type LogHeader = [
+    kind: 'h1' | 'h2',
+    message: [header: string] | [header: string, message: string]
+];
+type LogParagraph =
+    | [kind: 'p', message: string, marginOffset: number]
+    | [kind: 'p', message: string];
+type LogCommandDefinition =
+    | [kind: 'd' | 'cd', message: [word: string, definition: string], marginOffset: number]
+    | [kind: 'd' | 'cd', message: [word: string, definition: string]];
+type LogSyntax = [kind: 's', message: [commands: string[], args: string]];
+type LogCommandExample = [kind: 'e', message: [command: string, example: string]];
+
+const _leftLogMargin = 3;
+const _defaultIndent = 3;
+const _maxLogLength = 70;
+const _colorText = PrinterColor.colorText;
+
+export class Printer {
+    static print(logs: Log[]) {
+        logs.forEach((log) => {
+            printLog(log);
+        });
+    }
+
+    static log(msg: string, padding = 0) {
+        console.log(`${createFixedWidthSentences(msg, padding)}`);
+    }
+
+    static debug(...args: any[]) {
+        const stack = Error('').stack;
+        if (!stack) return;
+        const [offender, filePath, lineNumber] = getStackInfo(stack);
+        this.print([
+            null,
+            null,
+            ['h1', [';br;DEBUG']],
+            ['p', `;br;${'─'.repeat(65)};x;\n`],
+            null,
+        ]);
+        [...args].forEach((a) =>
+            console.log(inspect(a, { showHidden: true, depth: null, colors: true }))
+        );
+        this.print([
+            null,
+            ['p', `;br;${'─'.repeat(65)};x;\n`],
+            [
+                'p',
+                `;g;Exec ;x;by ;g;${offender || 'root'} ;x;in file ;g;${pathBasename(
+                    filePath
+                )} ;x;at line ;m;${lineNumber}`,
+            ],
+            null,
+            null,
+        ]);
+        console.log('');
+    }
+}
+
+function printLog(log: Log) {
+    if (log == null) return console.log('');
+
+    const [kind, message, padding] = log;
+    const realPadding = padding ?? 0;
+
+    if (kind == '') {
+        return console.log(applyLogMargin(_colorText(message)));
+    }
+
+    if (kind == 'h1' || kind == 'h2') {
+        const [header, headerMsg] = message;
+        if (
+            header.length > _maxLogLength ||
+            (headerMsg && headerMsg.length + header.length > _maxLogLength)
+        ) {
+            throw Error('headers cannot be larger than max log length');
+        }
+        return console.log(
+            _colorText(
+                applyLogMargin(
+                    `${kind == 'h1' ? ';bw;' : ';b;'}${header}:;x; ` + (headerMsg ?? '')
+                )
+            )
+        );
+    }
+
+    if (kind == 'p') {
+        return console.log(
+            _colorText(
+                createFixedWidthSentences(`;bk;${message}`, _defaultIndent + realPadding).join(
+                    '\n'
+                )
+            )
+        );
+    }
+
+    if (kind == 'e') {
+        const [command, example] = message;
+        return console.log(
+            applyLogMargin(
+                _colorText(`;by;wak ;c;-${command} ;y;${example}`),
+                _leftLogMargin + _defaultIndent
+            )
+        );
+    }
+
+    if (kind == 's') {
+        const [commands, args] = message;
+        return console.log(
+            applyLogMargin(
+                _colorText(
+                    `;by;wak ;bk;[${commands
+                        .map((c) => `;c;-${c};x;`)
+                        .join(' ;bk;| ')};bk;] ;y;${args};x;`
+                ),
+                _leftLogMargin + _defaultIndent
+            )
+        );
+    }
+
+    if (kind == 'd' || kind == 'cd') {
+        return printDefinitionLog(log);
+    }
+
+    if (kind == '') {
+        return console.log(_colorText(applyLogMargin(message)));
+    }
+
+    throw Error('invalid log');
+}
+
+function printDefinitionLog(log: LogCommandDefinition) {
+    const [kind, message, offset] = log;
+    const [word, definition] = message;
+    const defaultOffset = offset ?? 0;
+    const wordLength = word.length;
+    const [firstSentence, ...leftoverSentences] = createFixedWidthSentences(
+        definition,
+        0,
+        _maxLogLength - wordLength - 10
+    );
+    const newFirst = applyLogMargin(
+        `${kind == 'cd' ? ';c;-' : ';y;'}${word}   ;x;${firstSentence.trim()}`,
+        _leftLogMargin + _defaultIndent + defaultOffset
+    );
+
+    const paddedSentences = leftoverSentences.map((s) =>
+        applyLogMargin(
+            s,
+            wordLength + _defaultIndent * 2 + defaultOffset + (kind == 'cd' ? 1 : 0)
+        )
+    );
+
+    console.log(_colorText([newFirst, ...paddedSentences].join('\n')));
+}
+
+function createFixedWidthSentences(text: string, margin = 0, logLength = _maxLogLength) {
+    const words = text.split(' ');
+
+    const paragraph = [];
+    const stripColors = PrinterColor.stripColorCodes;
+    let sentence = '';
+
+    while (words.length) {
+        const word = stripColors(words[0]);
+        if (word.length + stripColors(sentence).length <= logLength) {
+            sentence += `${words[0]} `;
+            words.splice(0, 1);
+            if (!words.length) {
+                paragraph.push(sentence.trim());
+            }
+            continue;
+        }
+        paragraph.push(sentence.trim());
+        sentence = '';
+    }
+    return paragraph.map((v) => applyLogMargin(v, _leftLogMargin + margin));
+}
+
+function applyLogMargin(text: string, leftMargin = _leftLogMargin) {
+    return `${' '.repeat(leftMargin)}${text}`;
+}
+
+function getStackInfo(stack: string) {
+    const stackLines = stack.split('\n');
+    if (!stackLines.length || stackLines[1].trim().indexOf('at ') != 0) {
+        throw Error('not a stack trace');
+    }
+    const hasSourceMap = stack.includes('.ts:');
+    const pathLines = stackLines.map((l) => l.trim().replace('file:///', ''));
+    // Remove "Error" and Logger{} lines
+    pathLines.splice(0, 2);
+    const execLine = pathLines[0];
+    const offenderFunc = execLine.includes(' (')
+        ? execLine.split(' (')[0].replace('at ', '')
+        : '';
+    const [path, lineNumber] = execLine
+        .split(hasSourceMap ? ':\\' : ':/')[1]
+        .split(':')
+        .map((v) => v.replace(')', ''));
+    return [offenderFunc, pathBasename(path), lineNumber] as const;
+}

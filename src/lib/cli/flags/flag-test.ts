acf8c2fc600a03adfcc74c01909ced8efabe1c1d
Jaeiya <exanix@gmail.com>
2023-06-06 17:54:51 -0700

    fix(flag-test): forgot to remove old help dep

--- src/lib/cli/flags/flag-test.ts
+++ src/lib/cli/flags/flag-test.ts
@@ -1,8 +1,5 @@
-import { Help } from '../../help.js';
 import { Log } from '../../printer/printer.js';
 import { CLIFlag, CLIFlagName, CLIFlagType } from '../cli.js';
-
-const { h1, nl } = Help.display;
 
 export class TestFlag extends CLIFlag {
     name: CLIFlagName = ['!', '!'];
@@ -18,12 +15,6 @@ export class TestFlag extends CLIFlag {
             ['p', 'Do ;m;NOT ;bk;execute this command unless you know what it does.'],
         ];
     }
-
-    helpDisplay: string[] = [
-        h1(`Test Command`),
-        nl(`Do ;m;not ;x;execute this command unless you know what it does.`),
-        '',
-    ];
 
     exec() {
         //

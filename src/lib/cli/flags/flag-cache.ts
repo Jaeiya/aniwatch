1118f4f5677868cd8a485bec3699a00e50eb12aa
Jaeiya <exanix@gmail.com>
2023-07-25 14:00:56 -0700

    feat(flag-cache): combine cache `-rebuild` functionality
    closes #16

--- src/lib/cli/flags/flag-cache.ts
+++ src/lib/cli/flags/flag-cache.ts
@@ -1,11 +1,11 @@
-import { CLIFlag, CLIFlagName, CLIFlagType } from '../cli.js';
+import { CLI, CLIFlag, CLIFlagName, CLIFlagType } from '../cli.js';
 import { Kitsu } from '../../kitsu/kitsu.js';
 import { Log, Printer } from '../../printer/printer.js';
 import { Config } from '../../config.js';
 
 export class CacheFlag extends CLIFlag {
     name: CLIFlagName = ['c', 'cache'];
-    type: CLIFlagType = 'simple';
+    type: CLIFlagType = 'multiArg';
 
     helpAliases: string[] = [
         ...this.name,
@@ -14,43 +14,105 @@ export class CacheFlag extends CLIFlag {
         'get cache',
         'lookup cache',
         'list cache',
+        'rebuild cache',
+        'reload cache',
+        'load cache',
     ];
 
     shortHelpDisplay = 'Displays all currently Cached information.';
 
     getHelpLogs(): Log[] {
         return [
-            ['h1', ['Display Cache']],
-            ['p', 'This flag allows you to display the currently saved cache information.'],
+            ['h1', ['Cache']],
+            [
+                'p',
+                'This flag allows you to display a list of all anime within your cache ' +
+                    'or rebuild the cache from your Kitsu watch list.',
+            ],
             null,
         ];
     }
 
+    getSyntaxHelpLogs(): Log[] | null {
+        return [
+            ['h2', ['Syntax']],
+            ['s', ['c', 'cache'], '<info|rebuild>'],
+            null,
+            ['h2', ['Details']],
+            ['d', ['info', 'List all anime within the cache.'], 3],
+            null,
+            ['d', ['rebuild', 'Re-build cache from your Kitsu watch list.']],
+            null,
+            ['h2', ['Examples']],
+            ['e', ['c', 'info']],
+            ['e', ['c', 'rebuild']],
+            ['e', ['cache', 'info']],
+            ['e', ['cache', 'rebuild']],
+        ];
+    }
+
     exec(): void | Promise<void> {
-        const cache = Kitsu.animeCache;
-        const fileBindingCount = Config.getKitsuProp('fileBindings').length;
+        const [arg] = CLI.nonFlagArgs;
 
-        Printer.print([
-            null,
-            null,
-            ['h3', ['Anime Cache Info']],
-            null,
-            ['py', ['Cached Anime', `;m;${cache.length}`], 1],
-            ['py', ['File Bindings', `;m;${fileBindingCount}`]],
-            null,
-            ['h3', ['Cached Anime']],
-            ['hl', 'bk', 70],
-        ]);
+        if (CLI.nonFlagArgs.length > 1) {
+            Printer.printError(
+                'Make sure you use the correct syntax, as shown below:',
+                'Too Many Arguments'
+            );
+            return this.printSyntax();
+        }
 
-        for (const c of cache) {
-            Printer.print([
-                ['py', ['id', `;y;${c.libID}`], 6],
-                ['py', ['title_jp', `${c.jpTitle}`]],
-                ['py', ['title_en', `${c.enTitle}`]],
-                ['py', ['Progress', `;g;${c.epProgress} ;by;/ ;m;${c.epCount || 'Unknown'}`]],
-                ['', `;c;link: ;x;https://kitsu.io/anime/${c.slug}`, 7],
-                ['hl', 'bk', 70],
-            ]);
+        if (arg != 'info' && arg != 'rebuild') {
+            Printer.printError(
+                'Make sure you use the correct syntax, as shown below:',
+                'Invalid Argument'
+            );
+            return this.printSyntax();
+        }
+
+        if (arg == 'info') {
+            return showCacheInfo();
+        }
+
+        if (arg == 'rebuild') {
+            return rebuildCache();
         }
     }
+}
+
+function showCacheInfo() {
+    const cache = Kitsu.animeCache;
+    const fileBindingCount = Config.getKitsuProp('fileBindings').length;
+
+    Printer.print([
+        null,
+        null,
+        ['h3', ['Anime Cache Info']],
+        null,
+        ['py', ['Cached Anime', `;m;${cache.length}`], 1],
+        ['py', ['File Bindings', `;m;${fileBindingCount}`]],
+        null,
+        ['h3', ['Cached Anime']],
+        ['hl', 'bk', 70],
+    ]);
+
+    for (const c of cache) {
+        Printer.print([
+            ['py', ['id', `;y;${c.libID}`], 6],
+            ['py', ['title_jp', `${c.jpTitle}`]],
+            ['py', ['title_en', `${c.enTitle}`]],
+            ['py', ['Progress', `;g;${c.epProgress} ;by;/ ;m;${c.epCount || 'Unknown'}`]],
+            ['', `;c;link: ;x;https://kitsu.io/anime/${c.slug}`, 7],
+            ['hl', 'bk', 70],
+        ]);
+    }
+}
+
+async function rebuildCache() {
+    Printer.print([null]);
+    const stopLoader = Printer.printLoader('Rebuilding Cache');
+    const { cachedAnimeCount } = await Kitsu.rebuildCache();
+    stopLoader();
+    Printer.print([['h3', ['Rebuilding Cache']]]);
+    Printer.printInfo(`;bg;${cachedAnimeCount} ;g;Anime Reloaded`, 'Success', 3);
 }
--- src/lib/cli/flags/flags.ts
+++ src/lib/cli/flags/flags.ts
@@ -3,7 +3,6 @@ import { DirInfoFlag } from './flag-dir-size.js';
 import { FindAnimeFlag } from './flag-find-anime.js';
 import { HelpFlag } from './flag-help.js';
 import { ProfileFlag } from './flag-profile.js';
-import { RebuildCacheFlag } from './flag-rebuild-cache.js';
 import { RebuildProfileFlag } from './flag-rebuild-profile.js';
 import { RSSFeedFlag } from './flag-rss-feed.js';
 import { CacheFlag } from './flag-cache.js';
@@ -19,7 +18,6 @@ export const Flags = [
     ProfileFlag,
     RebuildProfileFlag,
     CacheFlag,
-    RebuildCacheFlag,
     ColorFlag,
     DirInfoFlag,
     CleanFlag,

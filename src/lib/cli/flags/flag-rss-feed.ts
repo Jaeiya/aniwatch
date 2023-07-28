ed0bd8e6910b34f2dcedbfd8d77f9f044238c3ac
Jaeiya <exanix@gmail.com>
2023-07-28 14:24:38 -0700

    upd(flag-rss): replace bitrate with raw file display

--- src/lib/cli/flags/flag-rss-feed.ts
+++ src/lib/cli/flags/flag-rss-feed.ts
@@ -77,13 +77,13 @@ export class RSSFeedFlag extends CLIFlag {
             ['py', ['Query', `;g;${cli.nonFlagArgs.join(' ')}`], 2],
             ['py', ['Entries', `${result.entryCount}`]],
             ['', `;c;RSS: ;x;${result.rss}`, 7],
+            ['', `;c;Raw: ;y;${result.raw}`, 7],
             null,
             ['h2', ['Latest Entry']],
             ['py', ['Title', `${result.title}`], 2],
             ['py', ['FanSub', `${result.fansub}`], 1],
             ['py', ['Episode', `${result.episode}`]],
             ['py', ['Season', `${result.season ?? ';m;unspecified'}`], 1],
-            ['py', ['BitRate', `${result.bitrate ?? ';m;unknown'}`]],
             null,
         ]);
     }
